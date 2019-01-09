a01aa2b52a206af370d1d713c3799cc307e98f41
Igor Freitas <igorfreitas@MacBook-Pro-de-Igor.local>
2019-01-08 22:07:39 -0300

    Removing unused controllers and routes

--- routes/address.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const address = app.controllers.address
-    const auth = require('../middlewares/auth')
-    
-    app.route('/api/address')
-        .get(auth(app), address.findAddressByCep)
-}
